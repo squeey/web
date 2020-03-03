a51aa9dd8eaf7d53d0253fcacf0a2864754e0054
code-dart <georgeokangaonline@gmail.com>
2020-03-03 11:52:20 +0100

    Adding main files to repo

---
+++ js/config.js
@@ -0,0 +1,7 @@
+requirejs.config({
+    baseUrl: '../js',
+    paths: {
+        jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min',
+        awesome: 'https://kit.fontawesome.com/7696dd4075'
+    }
+})
